5d33fe8101bfd90d643069307163e695142c52b8
ivanbrennan <ivan.brennan@gmail.com>
2015-12-30 15:36:29 -0500

    comments are cool

---
+++ prog5.c
@@ -0,0 +1,20 @@
+/* This program adds two integer values
+ * and displays the results              */
+
+#include <stdio.h>
+
+int main (void)
+{
+	// Declare variables
+	int value1, value2, sum;
+
+	// Assign values and calculate their sum
+	value1 = 50;
+	value2 = 25;
+	sum = value1 + value2;
+
+	// Display the result
+	printf ("The sum of %i and %i is %i\n", value1, value2, sum);
+
+	return 0;
+}
