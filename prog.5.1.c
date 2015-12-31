102257d8fb58b2814d44ae4ddc2ada58de17d3e2
ivanbrennan <ivan.brennan@gmail.com>
2015-12-31 12:57:10 -0500

    eigth triangular number

---
+++ prog.5.1.c
@@ -0,0 +1,15 @@
+// Program to calculate the eigth triangular number
+
+#include <stdio.h>
+
+int main (void)
+{
+	int triangularNumber;
+
+	triangularNumber = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8;
+
+	printf ("The eigth triangular number is %i\n",
+			triangularNumber);
+
+	return 0;
+}
