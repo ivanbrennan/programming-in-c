6dbf0c9a5b791a4e50fd94683d07f0f9e011eed7
ivanbrennan <ivan.brennan@gmail.com>
2015-12-31 16:55:44 -0500

    switch

---
+++ prog.6.9.c
@@ -0,0 +1,37 @@
+/* Program to evaluate simple expressions of the form
+               number  operator  number                 */
+
+#include <stdio.h>
+
+int main (void)
+{
+	float	value1, value2;
+	char	operator;
+
+	printf ("Type in your expression.\n");
+	scanf  ("%f %c %f", &value1, &operator, &value2);
+
+	switch (operator)
+	{
+		case '+':
+			printf ("%.2f\n", value1 + value2);
+			break;
+		case '-':
+			printf ("%.2f\n", value1 - value2);
+			break;
+		case '*':
+			printf ("%.2f\n", value1 * value2);
+			break;
+		case '/':
+			if ( value2 == 0 )
+				printf ("Division by zero.\n");
+			else
+				printf ("%.2f\n", value1 / value2);
+			break;
+		default:
+			printf ("Unknown operator.\n");
+			break;
+	}
+
+	return 0;
+}
