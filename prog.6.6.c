c1839babcc9a7794d2c7314e1bbf36ab104a1373
ivanbrennan <ivan.brennan@gmail.com>
2015-12-31 16:05:21 -0500

    else if

---
+++ prog.6.6.c
@@ -0,0 +1,22 @@
+// Program to implement the sign function
+
+#include <stdio.h>
+
+int main (void)
+{
+	int number, sign;
+
+	printf ("Please type in a number: ");
+	scanf  ("%i", &number);
+
+	if ( number < 0 )
+		sign = -1;
+	else if ( number == 0 )
+		sign = 0;
+	else		// Must be positive
+		sign = 1;
+
+	printf ("Sign = %i\n", sign);
+
+	return 0;
+}
