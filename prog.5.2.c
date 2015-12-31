46c4092d397b022791d460340357346215316c91
ivanbrennan <ivan.brennan@gmail.com>
2015-12-31 13:10:05 -0500

    200th triangular number

---
+++ prog.5.2.c
@@ -0,0 +1,18 @@
+/* Program to calculate the 200th triangular number
+   Introduction of the for statement                 */
+
+#include <stdio.h>
+
+int main (void)
+{
+	int n, triangularNumber;
+
+	triangularNumber = 0;
+
+	for ( n = 1; n <= 200; n = n + 1 )
+		triangularNumber = triangularNumber + n;
+
+	printf ("The 200th triangular number is %i\n", triangularNumber);
+
+	return 0;
+}
