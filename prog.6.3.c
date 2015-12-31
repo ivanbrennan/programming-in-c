b3ccf7d465573c436d13a1c3ba07300f1838d1cd
ivanbrennan <ivan.brennan@gmail.com>
2015-12-31 15:43:06 -0500

    if if even odd

---
+++ prog.6.3.c
@@ -0,0 +1,21 @@
+// Program to determine if a number is even or odd
+
+#include <stdio.h>
+
+int main (void)
+{
+	int number_to_test, remainder;
+
+	printf ("Enter your number to be tested.: ");
+	scanf  ("%i", &number_to_test);
+
+	remainder = number_to_test % 2;
+
+	if ( remainder == 0 )
+		printf ("The number is even.\n");
+
+	if ( remainder != 0 )
+		printf ("The number is odd.\n");
+
+	return 0;
+}
