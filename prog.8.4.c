09688ae6bd0dd48e0cfeaa6d2e9fed00952167c3
ivanbrennan <ivan.brennan@gmail.com>
2016-01-01 18:16:22 -0500

    triangular number function

---
+++ prog.8.4.c
@@ -0,0 +1,22 @@
+// Function to calculate the nth triangular number
+
+#include <stdio.h>
+
+void calculateTriangularNumber (int n)
+{
+	int i, triangularNumber = 0;
+
+	for ( i = 1; i <= n; ++i )
+		triangularNumber += i;
+
+	printf ("Triangular number %i is %i\n", n, triangularNumber);
+}
+
+int main (void)
+{
+	calculateTriangularNumber (10);
+	calculateTriangularNumber (20);
+	calculateTriangularNumber (50);
+
+	return 0;
+}
