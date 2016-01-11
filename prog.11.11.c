c36b40a6cbf750821f478226948b8f5759e64feb
ivanbrennan <ivan.brennan@gmail.com>
2016-01-11 07:32:22 -0500

    use pointer to sum elements of array

---
+++ prog.11.11.c
@@ -0,0 +1,25 @@
+// Function to sum the elements of an integer array
+
+#include <stdio.h>
+
+int arraySum (int array[], const int n)
+{
+	int sum = 0, *ptr;
+	int * const arrayEnd = array + n;
+
+	for ( ptr = array; ptr < arrayEnd; ++ptr ) {
+		sum += *ptr;
+	}
+
+	return sum;
+}
+
+int main (void)
+{
+	int arraySum (int array[], const int n);
+	int values[10] = { 3, 7, -9, 3, 6, -1, 7, 9, 1, -5 };
+
+	printf ("The sum is %i\n", arraySum (values, 10));
+
+	return 0;
+}
