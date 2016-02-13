87520b576e321c9aeb66a49a55b75e67810cb3c5
ivanbrennan <ivan.brennan@gmail.com>
2016-02-13 13:14:38 -0500

    copy from command line args

---
+++ prog.17.1.c
@@ -0,0 +1,35 @@
+// Program to copy on file to another -- version 2
+
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+	FILE	*in, *out;
+	int		c;
+
+	if ( argc != 3 ) {
+		fprintf (stderr, "Need two file names\n");
+		return 1;
+	}
+
+	if ( (in = fopen (argv[1], "r")) == NULL ) {
+		fprintf (stderr, "Can't read %s.\n", argv[1]);
+		return 2;
+	}
+
+	if ( (out = fopen (argv[2], "w")) == NULL ) {
+		fprintf (stderr, "Can't write %s.\n", argv[2]);
+		return 3;
+	}
+
+	while ( (c = getc (in)) != EOF ) {
+		putc (c, out);
+	}
+
+	printf ("File has been copied.\n");
+
+	fclose (in);
+	fclose (out);
+
+	return 0;
+}
